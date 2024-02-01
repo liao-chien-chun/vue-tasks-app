f985d27922f0a8c8936d8f9916543e7bbfe65023
Eric Liao <d1044171009@gmail.com>
2024-02-01 22:17:46 +0800

    feat: 完成新增任務功能/完成點擊可進行編輯與退出功能

--- src/pages/TasksPage.vue
+++ src/pages/TasksPage.vue
@@ -4,10 +4,8 @@
       <div class="row">
         <div class="col-md-8 offset-md-2">
           <!-- Add new Task -->
-          <div class="relative">
-            <input type="text" class="form-control form-control-lg padding-right-lg"
-                placeholder="+ Add new task. Press enter to save." />
-          </div>
+          <NewTask @added="handleAddedTask"/>
+
           <!-- List of uncompleted tasks -->
           <Tasks :tasks="uncompletedTasks" />
 
@@ -31,8 +29,9 @@
 <script setup>
   // 匯入 hook
   import { onMounted, ref, computed } from "vue";
-  import { allTasks } from "../http/task-api";
+  import { allTasks, createTask } from "../http/task-api";
   import Tasks from "../components/tasks/Tasks.vue"
+  import NewTask from "../components/tasks/NewTask.vue"
 
   // 建立一個保存任務的變數
   const tasks = ref([])
@@ -58,4 +57,9 @@
   )
 
   const showCompletedTasks = ref(false)
+
+  const handleAddedTask = async (newTask) => {
+    const { data: createdTask } = await createTask(newTask)
+    tasks.value.unshift(createdTask.data)
+  }
 </script>
