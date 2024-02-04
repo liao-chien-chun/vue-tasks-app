239121620a8fae5a0ab241bd7bee5720109101de
Eric Liao <d1044171009@gmail.com>
2024-02-04 20:45:32 +0800

    feat: 加入導航守護

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,23 @@
 import { createRouter, createWebHistory } from "vue-router";
 import routes from "./routes"
 
-export default createRouter({
+const router =  createRouter({
   // routes: routes,
   // 也可簡化成
   routes,
   history: createWebHistory(),
   // linkActiveClass: 'active'
 });
+
+// 全域
+// to  目標位置, from 當前位置
+router.beforeEach((to, from) => {
+  console.log("Global before each", to, from)
+  // return false
+  if (to.path === '/tasks') {
+    return { name: "login" };
+  }  
+})
+
+
+export default router
