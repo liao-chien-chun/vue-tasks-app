2ccd268a1406ed127c0d0868729a534bd345372a
Eric Liao <d1044171009@gmail.com>
2024-02-05 20:57:24 +0800

    feat: 加入下拉式選單隱藏登出按鈕

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -29,8 +29,13 @@
             </li>
           </template>
           <template v-else>
-            <li class="nav-item">
-              <a href="#" class="btn btn-outline-secondary ms-2" @click.prevent="logout">Logout</a>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" :class="toggleClass" @click.prevent="toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                {{ store.user.name }}
+              </a>
+              <ul class="dropdown-menu" :class="toggleClass">
+                <li><a href="#" class="dropdown-item" @click.prevent="logout">Logout</a></li>
+              </ul>
             </li>
           </template>
           </ul>
@@ -40,17 +45,24 @@
 </template>
 
 <script setup>
+import { ref, computed } from "vue"
 import { useRouter } from "vue-router";
 import { useAuthStore } from "../stores/auth";
 
 
 const router = useRouter()
 const store = useAuthStore()
+const isOpen = ref(false)
 
 const logout = async () => {
   await store.handleLogout()
+  isOpen.value = false
   router.push({ name: 'login' })
 }
+
+const toggle = () => isOpen.value = !isOpen.value
+
+const toggleClass = computed(() => isOpen.value === true ? 'show' : '')
 
 </script>
 
